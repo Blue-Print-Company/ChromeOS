389b4f55ed48beab72211bdd4643188aa13d4d1d
OctonalXX <54666576+OctonalXX@users.noreply.github.com>
2022-02-02 12:48:03 +0800

    TweaksV2

--- fetchimage.sh
+++ fetchimage.sh
@@ -2,10 +2,10 @@
 echo "Fetching Image Link..."
 linkdl=$(curl -v --stderr - https://cros-updates-serving.appspot.com/ | grep -o 'https[^"]*'"$1"'[^"]*zip' | tail -1)
 echo "Downloading From $linkdl"
-curl -L "${linkdl}" -output Img.bin.zip
+curl -L ${linkdl} -output Img.bin.zip
 
 # Process Files
-#tes
+#test
 echo HereEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 file Img.bin.zip
 7z x Img.bin.zip
